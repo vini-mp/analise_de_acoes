9a17612229ab7548fdea06694f45b75af8c64001
Vini <viniciusmarcospinheiro@hotmail.com>
2026-08-31 19:38:27 -0300

    Implementado carregamento de ações

--- main.py
+++ main.py
@@ -1,6 +1,50 @@
 """
-Deseneolvimento de um análisador 
-de ações na bolsa
+Análise de ações da bolsa
 
-
+pip install yfinance pandas matplotlib
 """
+
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+
+def carregar_acao(ticker, periodo="3y"):
+    ticker = ticker.upper()
+
+    if not ticker.endswith(".SA"):
+        ticker += ".SA"
+
+    dados = yf.download(
+        ticker,
+        period=periodo,
+        auto_adjust=False
+    )
+
+    return dados
+
+
+# Ação que queremos consultar
+ticker = "PETR4"
+
+# Importar dados
+dados = carregar_acao(ticker)
+
+# Mostrar os últimos dados
+print(dados.tail())
+
+# Criar gráfico
+plt.figure(figsize=(12, 6))
+
+plt.plot(
+    dados.index,
+    dados["Close"],
+    label=ticker
+)
+
+plt.title(f"Gráfico de {ticker}")
+plt.xlabel("Data")
+plt.ylabel("Preço (R$)")
+plt.grid(True)
+plt.legend()
+
+plt.show()
